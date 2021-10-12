eed2619cc9794fceb732a3b0e583f481f08a8a37
tsanders78 <36831852+tsanders78@users.noreply.github.com>
2021-10-12 02:48:05 -0400

    Update LoginActivity.java

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,14 +1,19 @@
 package com.codepath.rkpandey.instagramclonewithparse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.codepath.rkpandey.instagramclonewithparse.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String TAG = "Login Activity";
@@ -20,6 +25,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_login);
+
+        if (ParseUser.getCurrentUser() != null){
+             goMainActivity ();
+        }
 
         etUsername = findViewById (R.id.etUsername);
         etPassword = findViewById (R.id.etPassword);
@@ -40,6 +49,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.i (TAG, "Attempting to login user" + username);
+
+        ParseUser.logInInBackground (username, password, new LogInCallback () {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e !=null) {
+                    Log.e (TAG, "Issue with login", e);
+                    Toast.makeText (LoginActivity.this, "Issue with login", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                goMainActivity();
+                Toast.makeText (LoginActivity.this, "Success", Toast.LENGTH_SHORT).show ();
+
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 
 }
